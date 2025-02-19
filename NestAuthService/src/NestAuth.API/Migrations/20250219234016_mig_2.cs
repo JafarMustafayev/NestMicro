ed2ad0c3d67e000ce119dbd3385f995f97f233b6
JafarMustafayev <tu7gshbpm@code.edu.az>
2025-02-20 03:44:41 +0400

    Rename IsActive to UserStatus in AspNetUsers table
    -This migration `20250219234016_mig_2` updates the database schema by renaming the column `IsActive` to `UserStatus` in the `AspNetUsers` table.
    -The model snapshot has been adjusted to reflect this change, including the removal of the `IsActive` property and the addition of the `UserStatus` property.
    -Additionally, the Entity Framework Core version has been upgraded from `8.0.12` to `9.0.2`.

---
+++ NestAuthService/src/NestAuth.API/Migrations/20250219234016_mig_2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NestAuth.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "AspNetUsers",
+                newName: "UserStatus");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserStatus",
+                table: "AspNetUsers",
+                newName: "IsActive");
+        }
+    }
+}

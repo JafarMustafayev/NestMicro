34535579c6fb1d630c9234f76b46bff0190e9f76
JafarMustafayev <tu7gshbpm@code.edu.az>
2025-02-19 16:18:41 +0400

    Add conditional Swagger and Kestrel configuration
    Refactor the application to conditionally add Swagger generation and configure Kestrel server options based on the development environment.
    -The Kestrel server now listens on port 5005 in development and 5181 otherwise.
    -Additionally, integrate Consul registration into the application pipeline for improved service discovery and enhance overall code clarity and maintainability.

--- NestAPIGateway/src/NestAPIGateway/Program.cs
+++ NestAPIGateway/src/NestAPIGateway/Program.cs
@@ -7,7 +7,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddSwaggerGen();
+
+    builder.WebHost.ConfigureKestrel(serverOptions =>
+    {
+        serverOptions.ListenAnyIP(5005);
+    });
+}
+
 builder.Services.AddServices();
 builder.Services.AddOcelot().AddConsul();
 builder.Configuration.AddJsonFile
@@ -24,7 +34,9 @@ if (app.Environment.IsDevelopment())
 }
 app.UseAuthorization();
 app.MapControllers();
+
 await app.RegisterWithConsul(app.Lifetime);
+
 app.UseWhen(context => !context.Request.Path.StartsWithSegments("/health"), subApp =>
 {
     subApp.UseOcelot().Wait();
--- NestAuthService/src/NestAuth.API/Program.cs
+++ NestAuthService/src/NestAuth.API/Program.cs
@@ -4,13 +4,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddAuthServices();
 
-builder.WebHost.ConfigureKestrel(serverOptions =>
+if (builder.Environment.IsDevelopment())
 {
-    serverOptions.ListenAnyIP(5181);
-});
+    builder.Services.AddSwaggerGen();
+
+    builder.WebHost.ConfigureKestrel(serverOptions =>
+    {
+        serverOptions.ListenAnyIP(5181);
+    });
+}
 
 var app = builder.Build();
 

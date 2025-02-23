dfac2f0ef7d122fb290c1a05fe0f0099e10a8e87
JafarMustafayev <tu7gshbpm@code.edu.az>
2025-02-24 02:13:25 +0400

    Enhance IRepository and Repository with new methods
    Updated the IRepository<T> interface to include new methods for entity retrieval and management, supporting pagination, tracking, and related entity inclusion. Overloaded GetAllByExpression for improved filtering options. Added asynchronous methods like GetByIdAsync, GetByExpressionAsync, and CountAsync, along with methods for adding, deleting, and updating entities.
    
    Modified the Repository<T> class to implement the new interface methods, ensuring proper handling of parameters and query tracking options.

--- NestAuthService/src/NestAuth.API/Abstractions/IRepository.cs
+++ NestAuthService/src/NestAuth.API/Abstractions/IRepository.cs
@@ -4,13 +4,63 @@ public interface IRepository<T> where T : BaseEntityID, new()
 {
     DbSet<T> Table { get; }
 
-    public (IQueryable<T?> Items, int Count) GetAllByExpression(Expression<Func<T, bool>> expression);
+    public IQueryable<T?> GetAll(
+        bool isTracking = false,
+        Expression<Func<T, object>>? orderBy = null,
+        bool isDesingOrder = false,
+        List<Expression<Func<T, object>>>? includes = null,
+        List<(Expression<Func<T, object>> includes, Expression<Func<object, object>> thenIncludes)>? thenIncludes = null);
 
-    public Task<T?> GetByExpressionAsync(Expression<Func<T, bool>> expression);
+    public IQueryable<T?> GetAll(
+        int page = 1,
+        int take = 20,
+        bool isTracking = false,
+        Expression<Func<T, object>>? orderBy = null,
+        bool isDesingOrder = false,
+        List<Expression<Func<T, object>>>? includes = null,
+        List<(Expression<Func<T, object>> includes, Expression<Func<object, object>> thenIncludes)>? thenIncludes = null);
+
+    public (IQueryable<T?> Items, int Count) GetAllByExpression(
+        Expression<Func<T, bool>> expression,
+        int page = 1,
+        int take = 20,
+        bool isTracking = false,
+        Expression<Func<T, object>>? orderBy = null,
+        bool isDesingOrder = false,
+        List<Expression<Func<T, object>>>? includes = null,
+        List<(Expression<Func<T, object>> includes, Expression<Func<object, object>> thenIncludes)>? thenIncludes = null);
+
+    public (IQueryable<T?> Items, int Count) GetAllByExpression(
+        Expression<Func<T, bool>> expression,
+        bool isTracking = false,
+        Expression<Func<T, object>>? orderBy = null,
+        bool isDesingOrder = false,
+        List<Expression<Func<T, object>>>? includes = null,
+        List<(Expression<Func<T, object>> includes, Expression<Func<object, object>> thenIncludes)>? thenIncludes = null);
+
+    public Task<T?> GetByIdAsync(
+        string id,
+        bool isTracking = false,
+        List<Expression<Func<T, object>>>? includes = null,
+        List<(Expression<Func<T, object>> includes, Expression<Func<object, object>> thenincludes)>? thenincludes = null);
+
+    public Task<T?> GetByExpressionAsync(
+        Expression<Func<T, bool>> expression,
+        bool isTracking = false,
+        List<Expression<Func<T, object>>>? includes = null,
+        List<(Expression<Func<T, object>> includes, Expression<Func<object, object>> thenincludes)>? thenincludes = null);
 
     public Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
 
+    public Task<int> CountAsync(Expression<Func<T, bool>>? expression = null);
+
     Task AddAsync(T entity);
+
+    Task AddRangeAsync(ICollection<T> entities);
+
+    void Delete(T entity);
+
+    void DeleteRange(ICollection<T> entities);
 
     void Update(T entity);
 
--- NestAuthService/src/NestAuth.API/Repository/Repository.cs
+++ NestAuthService/src/NestAuth.API/Repository/Repository.cs
@@ -12,9 +12,126 @@ public class Repository<T> : IRepository<T> where T : BaseEntityID, new()
 
     public DbSet<T> Table => _context.Set<T>();
 
-    public (IQueryable<T?> Items, int Count) GetAllByExpression(Expression<Func<T, bool>> expression)
+    public IQueryable<T?> GetAll(
+        bool isTracking = false,
+        Expression<Func<T, object>>? orderBy = null,
+        bool isDesingOrder = false,
+        List<Expression<Func<T, object>>>? includes = null,
+        List<(Expression<Func<T, object>> includes, Expression<Func<object, object>> thenIncludes)>? thenIncludes = null)
     {
-        var query = Table.AsQueryable();
+        var query = isTracking ? Table.AsQueryable() : Table.AsNoTracking().AsNoTracking();
+
+        if (includes != null)
+        {
+            query = includes.Aggregate(query, (current, include) => current.Include(include));
+        }
+
+        if (thenIncludes != null)
+        {
+            query = thenIncludes.Aggregate(query, (current, include) => current.Include(include.includes).ThenInclude(include.thenIncludes));
+        }
+
+        if (orderBy != null)
+        {
+            query = isDesingOrder ? query.OrderByDescending(orderBy) : query.OrderBy(orderBy);
+        }
+
+        return query;
+    }
+
+    public IQueryable<T?> GetAll(
+        int page = 1,
+        int take = 20,
+        bool isTracking = false,
+        Expression<Func<T, object>>? orderBy = null,
+        bool isDesingOrder = false,
+        List<Expression<Func<T, object>>>? includes = null,
+        List<(Expression<Func<T, object>> includes, Expression<Func<object, object>> thenIncludes)>? thenIncludes = null)
+    {
+        var query = isTracking ? Table.AsQueryable() : Table.AsNoTracking().AsNoTracking();
+
+        if (includes != null)
+        {
+            query = includes.Aggregate(query, (current, include) => current.Include(include));
+        }
+
+        if (thenIncludes != null)
+        {
+            query = thenIncludes.Aggregate(query, (current, include) => current.Include(include.includes).ThenInclude(include.thenIncludes));
+        }
+
+        if (orderBy != null)
+        {
+            query = isDesingOrder ? query.OrderByDescending(orderBy) : query.OrderBy(orderBy);
+        }
+
+        query = query.Skip((page - 1) * take).Take(take);
+
+        return query;
+    }
+
+    public (IQueryable<T?> Items, int Count) GetAllByExpression(
+        Expression<Func<T, bool>> expression,
+        int page = 1,
+        int take = 20,
+        bool isTracking = false,
+        Expression<Func<T, object>>? orderBy = null,
+        bool isDesingOrder = false,
+        List<Expression<Func<T, object>>>? includes = null,
+        List<(Expression<Func<T, object>> includes, Expression<Func<object, object>> thenIncludes)>? thenIncludes = null)
+    {
+        var query = isTracking ? Table.AsQueryable() : Table.AsNoTracking().AsNoTracking();
+
+        if (includes != null)
+        {
+            query = includes.Aggregate(query, (current, include) => current.Include(include));
+        }
+
+        if (thenIncludes != null)
+        {
+            query = thenIncludes.Aggregate(query, (current, include) => current.Include(include.includes).ThenInclude(include.thenIncludes));
+        }
+
+        if (orderBy != null)
+        {
+            query = isDesingOrder ? query.OrderByDescending(orderBy) : query.OrderBy(orderBy);
+        }
+
+        query = query.Where(expression);
+        var count = query.Count();
+
+        if (count > 0)
+        {
+            query = query.Skip((page - 1) * take).Take(take);
+        }
+
+        return (query, count);
+    }
+
+    public (IQueryable<T?> Items, int Count) GetAllByExpression(
+        Expression<Func<T, bool>> expression,
+        bool isTracking = false,
+        Expression<Func<T, object>>? orderBy = null,
+        bool isDesingOrder = false,
+        List<Expression<Func<T, object>>>? includes = null,
+        List<(Expression<Func<T, object>> includes, Expression<Func<object, object>> thenIncludes)>? thenIncludes = null)
+    {
+        var query = isTracking ? Table.AsQueryable() : Table.AsNoTracking().AsNoTracking();
+
+        if (includes != null)
+        {
+            query = includes.Aggregate(query, (current, include) => current.Include(include));
+        }
+
+        if (thenIncludes != null)
+        {
+            query = thenIncludes.Aggregate(query, (current, include) => current.Include(include.includes).ThenInclude(include.thenIncludes));
+        }
+
+        if (orderBy != null)
+        {
+            query = isDesingOrder ? query.OrderByDescending(orderBy) : query.OrderBy(orderBy);
+        }
 
         query = query.Where(expression);
         var count = query.Count();
@@ -22,9 +139,44 @@ public class Repository<T> : IRepository<T> where T : BaseEntityID, new()
         return (query, count);
     }
 
-    public async Task<T?> GetByExpressionAsync(Expression<Func<T, bool>> expression)
+    public async Task<T?> GetByIdAsync(
+        string id,
+        bool isTracking = false,
+        List<Expression<Func<T, object>>>? includes = null,
+        List<(Expression<Func<T, object>> includes, Expression<Func<object, object>> thenincludes)>? thenincludes = null)
     {
-        var query = Table.AsQueryable();
+        var query = isTracking ? Table.AsQueryable() : Table.AsNoTracking().AsNoTracking();
+
+        if (includes != null)
+        {
+            query = includes.Aggregate(query, (current, include) => current.Include(include));
+        }
+
+        if (thenincludes != null)
+        {
+            query = thenincludes.Aggregate(query, (current, include) => current.Include(include.includes).ThenInclude(include.thenincludes));
+        }
+
+        return await query.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<T?> GetByExpressionAsync(
+       Expression<Func<T, bool>> expression,
+       bool isTracking = false,
+       List<Expression<Func<T, object>>>? includes = null,
+       List<(Expression<Func<T, object>> includes, Expression<Func<object, object>> thenincludes)>? thenincludes = null)
+    {
+        var query = isTracking ? Table.AsQueryable() : Table.AsNoTracking().AsNoTracking();
+
+        if (includes != null)
+        {
+            query = includes.Aggregate(query, (current, include) => current.Include(include));
+        }
+
+        if (thenincludes != null)
+        {
+            query = thenincludes.Aggregate(query, (current, include) => current.Include(include.includes).ThenInclude(include.thenincludes));
+        }
 
         return await query.FirstOrDefaultAsync(expression);
     }
@@ -34,14 +186,39 @@ public class Repository<T> : IRepository<T> where T : BaseEntityID, new()
         return await Table.AnyAsync(expression);
     }
 
+    public async Task<int> CountAsync(Expression<Func<T, bool>>? expression = null)
+    {
+        if (expression == null)
+        {
+            return await Table.CountAsync();
+        }
+
+        return await Table.CountAsync(expression);
+    }
+
     public async Task AddAsync(T entity)
     {
         await Table.AddAsync(entity);
     }
 
+    public async Task AddRangeAsync(ICollection<T> entities)
+    {
+        await Table.AddRangeAsync(entities);
+    }
+
     public void Update(T entity)
     {
         Table.Update(entity);
+    }
+
+    public void Delete(T entity)
+    {
+        Table.Remove(entity);
+    }
+
+    public void DeleteRange(ICollection<T> entities)
+    {
+        Table.RemoveRange(entities);
     }
 
     public async Task SaveChangesAsync()

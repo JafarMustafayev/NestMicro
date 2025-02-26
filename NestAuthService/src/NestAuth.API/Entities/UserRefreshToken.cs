5fc90f45ae79c7e5e82e28f5d325cd5615a709d5
JafarMustafayev <tu7gshbpm@code.edu.az>
2025-02-27 02:59:59 +0400

    Add UserSession management and update refresh tokens
    - Updated AppDbContext to include DbSet<UserSession>.
    - Modified UserRefreshToken to add SessionId and ReplacedByToken properties.
    - Introduced UserSession class with relevant properties and constructor.
    - Updated AppDbContextModelSnapshot to reflect new entities and relationships.
    - Created migration to alter UserRefreshTokens and add UserSessions table.
    - Updated migration designer file with new migration details.

--- NestAuthService/src/NestAuth.API/Entities/UserRefreshToken.cs
+++ NestAuthService/src/NestAuth.API/Entities/UserRefreshToken.cs
@@ -6,25 +6,26 @@ public class UserRefreshToken : BaseEntityID
 
     public string SessionId { get; set; }
 
-    public DateTime CreatedAt { get; set; }
-
     public string Token { get; set; }
+
+    public DateTime CreatedAt { get; set; }
 
     public DateTime Expires { get; set; }
 
-    public bool IsUsed { get; set; } = false;  // IsUsed is used to check if the token is used
+    public bool IsUsed { get; set; } = false;
 
-    public bool IsRevoked { get; set; } = false; // IsRevoked is used to check if the token is revoked
+    public bool IsRevoked { get; set; } = false;
 
-    public string ReplacedByTokenId { get; set; }  // ReplacedByTokenId is used to replace the token by token ID
+    public string CreatedByIp { get; set; }
 
-    public string CreatedByIp { get; set; }  // CreatedByIp is used to create the token by IP address of the user
-
-    public string RevokedByIp { get; set; }  // RevokedByIp is used to revoke the token by IP address of the user
+    public string RevokedByIp { get; set; }
+    public string ReplacedByTokenId { get; set; }
 
     public virtual AppUser User { get; set; }
 
     public virtual UserSession Session { get; set; }
+
+    public virtual UserRefreshToken ReplacedByToken { get; set; }
 
     public UserRefreshToken()
     {
